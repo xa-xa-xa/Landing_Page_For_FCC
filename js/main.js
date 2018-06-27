e8d7ba74c51d216ace7a2fa0268fdff5b4e3ab8b
xa-xa-xa <28684394+xa-xa-xa@users.noreply.github.com>
2018-06-26 17:00:51 -0700

    first try on git

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 
+// universal scroll // 
+$(function() {
+  // This will select everything with the class smoothScroll
+  // This should prevent problems with carousel, scrollspy, etc...
+  $('.smoothScroll').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      console.log(this.hash);
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top -50
+        }, 1000); // The number here represents the speed of the scroll in milliseconds
+        return false;
+      }
+    }
+  });
+});
+
+// hamburger Menu //
+const hamburgerMenu = function (x) {
+   x.classList.toggle("change");
+} 
